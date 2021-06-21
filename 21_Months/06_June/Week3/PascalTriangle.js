2139fdfb6d4df405e2c7b5af383e4468718808f1
Faj5097 <fab.jan@t-online.de>
2021-06-21 22:54:31 +0200

    June Week 3 Pascals Triangle

---
+++ 21_Months/06_June/Week3/PascalTriangle.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  let ans = [[1]];
+  let i = 1;
+
+  while (i < numRows) {
+    let arr = [1];
+    for (let j = 0; j < ans[i - 1].length - 1; j++) {
+      let left = ans[i - 1][j];
+      let right = ans[i - 1][j + 1];
+      arr.push(left + right);
+    }
+    arr.push(1);
+    console.log(arr);
+    ans.push(arr);
+    i++;
+  }
+
+  return ans;
+};
+
+console.log(generate(5));
